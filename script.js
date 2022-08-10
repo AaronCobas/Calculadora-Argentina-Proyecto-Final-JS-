ab1fa881df02bb4518079af140fefc99799fef02
Aaron Andrew Cobas <aaroncobas2017@gmail.com>
2022-08-10 09:57:45 -0300

    proyecto final

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ class Calculo {
     }
 }
 let calculos = []
+
+const formIVA = document.getElementById("formIVA")
+const ivaInput = document.getElementById("ivaInput")
+const divIVA = document.getElementById("divIVA")
+const formICL = document.getElementById("formICL")
+const iclInput = document.getElementById("iclInput")
+const divICL = document.getElementById("divICL")
 /* Muestra (o en caso de no haber, crea) un localStorage */
 if(localStorage.getItem("calculos")){
     calculos = JSON.parse(localStorage.getItem("calculos"))
@@ -17,21 +24,15 @@ function calculadoraIva(numero1){
 function calculadoraICL(numero2){
     return(numero2 * 1.75)
 }
-const formIVA = document.getElementById("formIVA")
-const ivaInput = document.getElementById("ivaInput")
-const divIVA = document.getElementById("divIVA")
-const formICL = document.getElementById("formICL")
-const iclInput = document.getElementById("iclInput")
-const divICL = document.getElementById("divICL")
 formIVA.addEventListener("submit", (iva1) => {
     iva1.preventDefault()
 let numeroCalcular = parseFloat(ivaInput.value)
 let resultadoIVA = calculadoraIva(numeroCalcular)
+divIVA.innerHTML = ""
 divIVA.innerHTML += `
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 18rem;" id="">
 <div class="card-body">
     <h5 class="card-title">${resultadoIVA}</h5>
-    <button class="btn btn-danger"><img src="https://img.icons8.com/pastel-glyph/20/FFFFFF/trash.png"></button>
 </div>
 </div>
 `
@@ -40,19 +41,15 @@ calculos.push(calculosIVA)
 localStorage.setItem("calculos", JSON.stringify(calculos))
 formIVA.reset() 
 })
-
-
-
-
 formICL.addEventListener("submit", (e1) => {
     e1.preventDefault()
 let numeroCalcularicl = parseFloat(iclInput.value)
 let resultadoICL = calculadoraICL(numeroCalcularicl)
+divICL.innerHTML = ""
 divICL.innerHTML += `
 <div class="card" style="width: 18rem;" id="">
 <div class="card-body">
     <h5 class="card-title">${resultadoICL}</h5>
-    <button class="btn btn-danger"><img src="https://img.icons8.com/pastel-glyph/20/FFFFFF/trash.png"></button>
 </div>
 </div>
 `
